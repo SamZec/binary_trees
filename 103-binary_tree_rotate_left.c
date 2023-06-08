a36238a425a645cd2d26961f5b85940865239d4b
SamZec <samzec4@gmail.com>
2023-06-08 16:13:42 +0000

    22. Rotate left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -4,7 +4,7 @@
  * binary_tree_rotate_left - performs a left-rotation on a binary tree
  * @tree: pointer to the root node of the tree to rotate
  *
- * Retrun:  pointer to the new root node of the tree once rotated
+ * Return:  pointer to the new root node of the tree once rotated
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
